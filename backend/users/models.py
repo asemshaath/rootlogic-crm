f75407abd6391e6c9efc3011f2d80d925947cd91
Asem Shaath <shaathasem@gmail.com>
2025-10-18 18:50:38 -0400

    backend: users app and orm created

---
+++ backend/users/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+import datetime
+from django.utils.text import slugify
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'organization'
+        verbose_name = 'Organization'
+        verbose_name_plural = 'Organizations'
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        self.updated_at = datetime.datetime.now()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class User(AbstractUser):
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True, blank=True)
+    role = models.CharField(max_length=50, choices=[('admin', 'Admin'), ('member', 'Member')])
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=["username"]
+    
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self) -> str:
+        return self.email
