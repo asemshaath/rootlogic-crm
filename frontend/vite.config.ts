0232d6bf1cdc88e3bf7c8925899a4cd16836ab53
Asem Shaath <shaathasem@gmail.com>
2025-10-21 20:19:47 -0400

    frontend: vite.config.ts change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT ?? '5173', 10) || 5173,
+    allowedHosts: ['*'],
+  },
+
 })
