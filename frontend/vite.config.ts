bea8d5c16611f455620a8d08b0e0f6e1d8924558
Asem Shaath <shaathasem@gmail.com>
2025-10-21 20:33:11 -0400

    frontend: fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT ?? '5173', 10) || 5173,
+    port: parseInt(process.env.PORT ?? '5173', 10),
+    allowedHosts: ['*'],
+    strictPort: true,
+    hmr: {
+      clientPort: 443, // ensure correct websocket port for HTTPS
+    },
+  },
+  preview: {
     allowedHosts: ['*'],
   },
-
 })
